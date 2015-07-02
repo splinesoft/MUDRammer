7e967abf057c492ee632bc94b1c1d13c1b89debb
Jonathan Hersh <jon@her.sh>
2015-07-01 23:05:20 -0700

    [Tests] disable socket connection tests
    It's neither reasonable nor fair to assume Discworld will be online. Networking tests should be stubbed with appropriate responses.

--- src/MRTests/MRSocketTests.m
+++ src/MRTests/MRSocketTests.m
@@ -46,81 +46,81 @@
     expect(sut.SSdelegate).to.equal(mockSocketDelegate);
 }
 
-- (void)testSocketConnectSendsConnectAndSSLCheck {
-    NSError *err;
-
-    [[mockSocketDelegate expect] mudsocketDidConnectToHost:sut];
-    [[mockSocketDelegate expect] mudsocketShouldAttemptSSL:sut];
-
-    BOOL connected = [sut.socket connectToHost:@"discworld.starturtle.net"
-                                        onPort:23
-                                   withTimeout:30
-                                         error:&err];
-
-    expect(connected).to.beTruthy();
-    expect(err).to.beNil();
-
-    [mockSocketDelegate verifyWithDelay:3];
-}
-
-- (void)testSocketDisconnectSendsDisconnect {
-    NSError *err;
-
-    [[mockSocketDelegate expect] mudsocket:sut didDisconnectWithError:OCMOCK_ANY];
-
-    BOOL connected = [sut.socket connectToHost:@"discworld.starturtle.net"
-                                        onPort:23
-                                   withTimeout:30
-                                         error:&err];
-
-    expect(connected).to.beTruthy();
-    expect(err).to.beNil();
-
-    [sut.socket disconnect];
-
-    [mockSocketDelegate verifyWithDelay:3];
-}
-
-- (void)testSocketParsesAttributedLines {
-    NSError *err;
-
-    [[mockSocketDelegate expect] mudsocket:sut
-             didReceiveAttributedLineGroup:[OCMArg checkWithBlock:^BOOL(id object) {
-
-        return [object isKindOfClass:[SSAttributedLineGroup class]]
-            && [((SSAttributedLineGroup *)object).textLines count] > 0;
-    }]];
-
-    BOOL connected = [sut.socket connectToHost:@"discworld.starturtle.net"
-                                        onPort:23
-                                   withTimeout:30
-                                         error:&err];
-
-    expect(connected).to.beTruthy();
-    expect(err).to.beNil();
-
-    [mockSocketDelegate verifyWithDelay:3];
-}
-
-- (void)testSocketParsesMSSPResponse {
-    NSError *err;
-
-    [[mockSocketDelegate expect] mudsocket:sut
-                          receivedMSSPData:[OCMArg checkWithBlock:^BOOL(id object) {
-
-        return [object isKindOfClass:[NSDictionary class]]
-            && [(NSDictionary *)object count] > 0;
-    }]];
-
-    BOOL connected = [sut.socket connectToHost:@"discworld.starturtle.net"
-                                        onPort:23
-                                   withTimeout:30
-                                         error:&err];
-
-    expect(connected).to.beTruthy();
-    expect(err).to.beNil();
-
-    [mockSocketDelegate verifyWithDelay:3];
-}
+//- (void)testSocketConnectSendsConnectAndSSLCheck {
+//    NSError *err;
+//
+//    [[mockSocketDelegate expect] mudsocketDidConnectToHost:sut];
+//    [[mockSocketDelegate expect] mudsocketShouldAttemptSSL:sut];
+//
+//    BOOL connected = [sut.socket connectToHost:@"discworld.starturtle.net"
+//                                        onPort:23
+//                                   withTimeout:30
+//                                         error:&err];
+//
+//    expect(connected).to.beTruthy();
+//    expect(err).to.beNil();
+//
+//    [mockSocketDelegate verifyWithDelay:3];
+//}
+//
+//- (void)testSocketDisconnectSendsDisconnect {
+//    NSError *err;
+//
+//    [[mockSocketDelegate expect] mudsocket:sut didDisconnectWithError:OCMOCK_ANY];
+//
+//    BOOL connected = [sut.socket connectToHost:@"discworld.starturtle.net"
+//                                        onPort:23
+//                                   withTimeout:30
+//                                         error:&err];
+//
+//    expect(connected).to.beTruthy();
+//    expect(err).to.beNil();
+//
+//    [sut.socket disconnect];
+//
+//    [mockSocketDelegate verifyWithDelay:3];
+//}
+//
+//- (void)testSocketParsesAttributedLines {
+//    NSError *err;
+//
+//    [[mockSocketDelegate expect] mudsocket:sut
+//             didReceiveAttributedLineGroup:[OCMArg checkWithBlock:^BOOL(id object) {
+//
+//        return [object isKindOfClass:[SSAttributedLineGroup class]]
+//            && [((SSAttributedLineGroup *)object).textLines count] > 0;
+//    }]];
+//
+//    BOOL connected = [sut.socket connectToHost:@"discworld.starturtle.net"
+//                                        onPort:23
+//                                   withTimeout:30
+//                                         error:&err];
+//
+//    expect(connected).to.beTruthy();
+//    expect(err).to.beNil();
+//
+//    [mockSocketDelegate verifyWithDelay:3];
+//}
+//
+//- (void)testSocketParsesMSSPResponse {
+//    NSError *err;
+//
+//    [[mockSocketDelegate expect] mudsocket:sut
+//                          receivedMSSPData:[OCMArg checkWithBlock:^BOOL(id object) {
+//
+//        return [object isKindOfClass:[NSDictionary class]]
+//            && [(NSDictionary *)object count] > 0;
+//    }]];
+//
+//    BOOL connected = [sut.socket connectToHost:@"discworld.starturtle.net"
+//                                        onPort:23
+//                                   withTimeout:30
+//                                         error:&err];
+//
+//    expect(connected).to.beTruthy();
+//    expect(err).to.beNil();
+//
+//    [mockSocketDelegate verifyWithDelay:3];
+//}
 
 @end
